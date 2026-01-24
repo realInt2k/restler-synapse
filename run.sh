b66f03a48a2d3cc9b42d083e35da74ca315751b6
int2k <dungduychinguyen@gmail.com>
2026-01-24 11:34:31 -0800

    first runs

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e  # exit immediately if any command fails
+
+case "$1" in
+  compile)
+    echo "Running compile..."
+    docker run --rm \
+        --network host \
+        -v $(pwd)/mydata:/data \
+        -v $(pwd)/restler-work:/work \
+        -w /work \
+        restler \
+        /RESTler/restler/Restler compile --api_spec /data/swagger.json
+    ;;
+  
+  test)
+    echo "Running tests..."
+    docker run --rm \
+        --network host \
+        -v $(pwd)/mydata:/data \
+        -v $(pwd)/restler-work:/work \
+        -w /work \
+        restler \
+        /RESTler/restler/Restler test --grammar_file /work/Compile/grammar.py --dictionary_file /work/Compile/dict.json --settings /work/Compile/engine_settings.json --no_ssl
+    ;;
+  
+  fuzz-lean)
+     echo "Running fuzz-lean..."
+     docker run --rm \
+        --network host \
+        -v $(pwd)/mydata:/data \
+        -v $(pwd)/restler-work:/work \
+        -w /work \
+        restler \
+        /RESTler/restler/Restler fuzz-lean --grammar_file /work/Compile/grammar.py --dictionary_file /work/Compile/dict.json --settings /work/Compile/engine_settings.json --no_ssl
+    ;;
+  *)
+    echo "Usage: $0 {compile|test}"
+    exit 1
+    ;;
+esac
+
